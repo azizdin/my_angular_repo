f971f9ecc695ed6bfe7b2d95484961e2471686a1
Abdul Aziz bin Din <aziz.din@cmasolutions.my>
2024-03-07 16:39:14 +0800

    using ccenter component

--- my-calculator/src/app/app.component.ts
+++ my-calculator/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SalaryDeductionComponent } from './features/salary-deduction/pages/salary-deduction/salary-deduction.component';
+import { CenterComponents } from './core/components/layout/intex';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SalaryDeductionComponent],
+  imports: [RouterOutlet, SalaryDeductionComponent, CenterComponents],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
